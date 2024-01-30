5f160fa78b62741e0eda04b09bedf8566362e7e5
saideepikaneeluru <158000517+saideepikaneeluru@users.noreply.github.com>
2024-01-29 20:03:44 -0500

    Add files via upload

--- question6.py
+++ question6.py
@@ -1,15 +1,21 @@
 def question6_1():
-    """
     answer = {
-        'equal_width': ['string', integer],
-        'equal_frequency': ['string', integer]
+        'equal_width': ['No change', 3],
+        'equal_frequency': ['No change', 3]
     }
-    """
     return answer
 
 def question6_2():
+    answer = {
+        'equal_width': ['No Change', 7],
+        'equal_frequency': ['No Change', 9]
+    }
     return answer
 
 def question6_3():
-    return answer 
+    answer = {
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['No Change', 9]
+    } 
+    return answer
 
--- question7.py
+++ question7.py
@@ -1,19 +1,14 @@
 def question7_1():
-    """
-    answer = 'string'
-    """
-    return answer
+    answer ='increase/decrease'
+    return answer 
 
 def question7_2():
-    """
-    answer = 'string'
-    """
+    answer = 'increase/decrease'
     return answer
 
 def question7_3():
-    """
-    answer = [(b1, e1), (b2, e2), ..., (bn, en)]
-    The representation of inifinity is 'infinity'. 
-    bi, ei are floats or 'infinity'. 
-    """
-    return answer 
+    answer = [('infinity', 0.),  
+              (1., 3.),              
+              (4., 5.),              
+              (6., 'infinity')]
+    return answer
