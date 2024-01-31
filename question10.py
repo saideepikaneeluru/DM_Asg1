54de830188da3cd13a21287ee711c7a9627e5074
saideepikaneeluru <158000517+saideepikaneeluru@users.noreply.github.com>
2024-01-30 22:53:23 -0500

    Update question10.py

--- question10.py
+++ question10.py
@@ -1,21 +1,17 @@
 def question10_1():
     answer = False
-    justify = 'Count data may also be influenced by noise, as it has the potential to introduce variability and impact the precision of the counts.'
     return answer
 
 def question10_2():
     answer = False
-    justify = 'The correlation between actual values typically falls within the range of -1 to 1 exclusively in the case of linear relationships. Otherwise, it can exhibit variations, suggesting either non-linear associations or the absence of linear correlation.'
     return answer
 
 def question10_3():
     answer = False
-    justify = 'Aggregating serves to decrease the dimensionality of a time series by summarizing data across intervals, thereby preserving essential patterns. On the other hand, sampling may overlook crucial details, resulting in a loss of information.'
     return answer
 
 def question10_4():
     answer = True
-    justify = 'Noise and outliers can be indistinguishable at times since both exhibit substantial deviations from the anticipated pattern, creating challenges in differentiation under specific circumstances.'
     return answer
 
 def question10_5():
